9dcc70ce905d4580ab9ce2a5acc75443ca49e749
Nirox <theo.mas677@gmail.com>
2026-01-24 18:25:58 +0100

    Simplify Sequelize initialization and update DB URL
    Updated the DATABASE_URL in config.js to use the 'postgresql' protocol. Refactored models/index.js to always initialize Sequelize using individual credentials from the BDD config, removing conditional logic for DATABASE_URL.

--- api/models/index.js
+++ api/models/index.js
@@ -1,32 +1,19 @@
 const { Sequelize } = require ("sequelize");
-const config  = require ('../config');
-
-// Use DATABASE_URL if available (Render provides this), otherwise use individual credentials
-const sequelize = config.DATABASE_URL 
-  ? new Sequelize(config.DATABASE_URL, {
-      dialect: 'postgres',
-      protocol: 'postgres',
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      },
-      define: {
-        timestamps: false
+const { BDD }  = require ('../config');
+const sequelize = new Sequelize(`postgres://${BDD.user}:${BDD.password}@${BDD.host}/${BDD.bdname}`
+,{
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
       }
-    })
-  : new Sequelize(`postgres://${config.BDD.user}:${config.BDD.password}@${config.BDD.host}/${config.BDD.bdname}`, {
-      dialect: 'postgres',
-      protocol: 'postgres',
-      dialectOptions: {
-        ssl: true,
-        native: true
-      },
-      define: {
-        timestamps: false
-      }
-    });
+    },
+    define:  {
+    	timestamps:false
+    }
+  });
 
 const db = {};
 
