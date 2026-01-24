f2c9b207dbd222cdd193d14d3d0b871d55c4f63b
Nirox <theo.mas677@gmail.com>
2026-01-24 19:50:46 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,10 +14,11 @@ let corsOptions = {
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-app.use(express.json());
+// Augmenter la limite à 10MB pour supporter les images base64
+app.use(express.json({ limit: '10mb' }));
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
