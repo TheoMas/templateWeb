659da8556cc7b5a43996cf31c528b061be052a3f
Nirox <theo.mas677@gmail.com>
2026-01-28 19:24:50 +0100

    Update favorites.routes.js

--- api/routes/favorites.routes.js
+++ api/routes/favorites.routes.js
@@ -6,9 +6,9 @@ module.exports = app => {
   // Toutes les routes favorites protégées par JWT
   router.use(authenticateJWT);
   router.post("/", favorites.addFavorite);
-  router.delete(":/userId/:pollutionId", favorites.removeFavorite);
-  router.get(":/userId", favorites.getUserFavorites);
-  router.get(":/userId/:pollutionId", favorites.isFavorite);
+  router.delete("/:userId/:pollutionId", favorites.removeFavorite);
+  router.get("/:userId", favorites.getUserFavorites);
+  router.get("/:userId/:pollutionId", favorites.isFavorite);
 
   app.use('/api/favorites', router);
 };
