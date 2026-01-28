1af249c1b7796bd1b5a4258f3b4615c93bc9d87b
Nirox <theo.mas677@gmail.com>
2026-01-28 18:14:04 +0100

    Update jwt-middleware.js

--- api/jwt-middleware.js
+++ api/jwt-middleware.js
@@ -3,7 +3,14 @@ const jwt = require('jsonwebtoken');
 const config = require('./config');
 
 function authenticateJWT(req, res, next) {
-  const token = req.cookies && req.cookies.access_token;
+  // Priorité à l'en-tête Authorization, sinon cookie
+  let token = null;
+  const authHeader = req.headers['authorization'];
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    token = authHeader.substring(7);
+  } else if (req.cookies && req.cookies.access_token) {
+    token = req.cookies.access_token;
+  }
   if (!token) {
     return res.status(401).json({ message: 'Token manquant' });
   }
